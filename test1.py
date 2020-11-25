30446f70ade8ce5ccc860dec804388764fc360d4
EUPHORIA1005 <ya.dima.nick@gmail.com>
2020-11-25 14:49:48 +0300

    Task 2

---
+++ test1.py
@@ -0,0 +1,16 @@
+def is_palindrome4(num):
+    one = num // 1000
+    two = num // 100 % 10
+    three = num // 10 % 10
+    four = num % 10
+    if one == four and two == three:
+        return(1)
+    else:
+        return(0)
+
+A = int(input())
+B = int(input())
+for i in range(A, B + 1):
+    if  is_palindrome4(i) == 1 :
+        print(i)
+
